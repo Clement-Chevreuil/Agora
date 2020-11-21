bccea1785e4a1c6007fdd747c8e7b5c466dffb20
Clement Chevreuil <clement.chev>
2020-11-21 14:00:19 +0100

    Commentaire V.1

--- js/main.js
+++ js/main.js
@@ -25,3 +25,18 @@ function myFunction()
   document.getElementById('diapo'+lol).className = "diapo3";
     lol = lol - 1;
 }
+
+function testHidden()
+{
+    var x = document.getElementById("test");
+
+    if (x.style.display === "none") 
+    {
+        x.style.display = "block";
+        $("html, body").animate({ scrollTop: 0}, 1000); 
+    } 
+    else 
+    {
+        x.style.display = "none";
+    }
+}
